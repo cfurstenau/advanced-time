89f63bd916540a14db72c3e5852ee0c6b1e00a90
OpenShift guest <charles.furstenau@wwtas.com>
2014-06-30 14:04:50 -0400

    astime 1

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
-var express = require('express');
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
 var bodyParser = require('body-parser');
-var app = express();
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+// required for passport
+app.use(session({ secret: 'ja8j42haf67ajw9dajkwa8a82hj4f6a2h4h2k8' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 //this makes things in the /public folder accessible
 app.use(express.static(__dirname + '/public'));
-//required to read POST body
-app.use(bodyParser.urlencoded({extended:true}));
 
-//handler for the login form
-app.post('/login', function(req, res) {
-    console.log(req.body);
-    if (req.body.username == 'user' && req.body.password == 'password'){
-      
-      //TODO: do some research in handling a JSON response (making a single page app)
-      res.send({success: true, message: ""});
-    } else {
-      res.send({
-        success: false,
-        errors: {
-          username: "Wrong username",
-          password: "Wrong password"
-        }
-      });
-    }
-});
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 
-var server = app.listen(process.env.PORT, function() {
+
+var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
 });
