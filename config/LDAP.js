166fac954319466e7f75726ff39cf0c5722e1f12
KeeganSullivan <Keegan.Sullivan@wwtas.com>
2014-08-08 10:04:44 -0400

    General LDAP.js and SQL.js
    Non Sensitive LDAP.js and SQL.js

---
+++ config/LDAP.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+
+	 url 			: "LDAP://url",
+     adminDn		: 'Dsitinguished Name i.e. CN=test,OU=Restricted,,DC=com',
+     adminPassword	: "password", 
+     searchBase		: "i.e ou=Users,dc=com",
+	 searchFilter	: "(sAMAccountName={{username}})"
+
+};
---
+++ config/sql.js
@@ -0,0 +1,6 @@
+module.exports = {
+    userName: 'project name',
+    password: 'password',
+    server: 'server',
+    options: {database: "name of SQL database", encrypt: true}
+  };
